76d53f1b6252dd0c84b65e6de6809c70f31f7ade
Sócrates Díaz <sdsxd1800@gmail.com>
2022-07-03 13:24:59 -0400

    Add condition to avoid jumping if player is not on floor while being idle.

--- scenes/player/Idle.gd
+++ scenes/player/Idle.gd
@@ -8,7 +8,7 @@ func enter(_msg := {}) -> void:
 func physics_update(_delta: float) -> void:
 	if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
 		state_machine.transition_to("Walk")
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and player.is_on_floor():
 		state_machine.transition_to("OnAir", {do_jump = true})
 
 	if not player.is_on_floor():
